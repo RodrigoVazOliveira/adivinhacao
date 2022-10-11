6f291922b12cf49e99015521ad5c4c11a9e41ea9
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-10 21:26:20 -0300

    Adicionar nova funcionalidade para adivinha

--- advinhacao.py
+++ advinhacao.py
@@ -1,3 +1,4 @@
+print("=================================")
 print("Bem vindo ao jogo de adivinhacao!")
 print("=================================")
 
@@ -5,9 +6,16 @@ numeric_secret = 42
 numeric_user = int(input("Digite um o seu numero: "))
 
 print("Voce digitou ", numeric_user)
-if numeric_secret == numeric_user:
+accept = numeric_secret == numeric_user
+numeric_great = numeric_user > numeric_secret
+numeric_minor = numeric_user < numeric_secret
+
+if accept:
     print("Voce acertou!")
 else:
-    print("Voce errou!")
+    if numeric_great:
+        print("Maior que o numero do sorteio")
+    elif numeric_minor:
+        print("O numero e o menor que sorteio")
 
 print("Fim do jogo!")
