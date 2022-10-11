f52ff559ce68a71ec6f284c264ae678e14c6a08a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-10 21:26:48 -0300

    Verificar nome de usuario no sistema

---
+++ user_system.py
@@ -0,0 +1,9 @@
+input_name_user = input("Por favor, digite o nome do usuario do sistema: ")
+if input_name_user == "Flavio":
+    print("Seja bem vindo Flavio!")
+elif input_name_user == "Nico":
+    print("Seja bem vindo Nico!")
+elif input_name_user == "Douglas":
+    print("Seja bem vindo Douglas!")
+else:
+    print("Usuario nao identificado")
