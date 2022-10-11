833ddca53725cf874de6ab0391b6fc212e6d1055
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-10 21:41:59 -0300

    Implementar repeticacao no programa

--- advinhacao.py
+++ advinhacao.py
@@ -3,19 +3,25 @@ print("Bem vindo ao jogo de adivinhacao!")
 print("=================================")
 
 numeric_secret = 42
-numeric_user = int(input("Digite um o seu numero: "))
 
-print("Voce digitou ", numeric_user)
-accept = numeric_secret == numeric_user
-numeric_great = numeric_user > numeric_secret
-numeric_minor = numeric_user < numeric_secret
+numeric_attempt = 3
+round_game = 1
 
-if accept:
-    print("Voce acertou!")
-else:
-    if numeric_great:
-        print("Maior que o numero do sorteio")
-    elif numeric_minor:
-        print("O numero e o menor que sorteio")
+while round_game <= numeric_attempt:
+    print("rodada", round_game, ", numero de tentativas: ", numeric_attempt)
+    numeric_user = int(input("Digite um o seu numero: "))
+    accept = numeric_secret == numeric_user
+    numeric_great = numeric_user > numeric_secret
+    numeric_minor = numeric_user < numeric_secret
+
+    print("Voce digitou ", numeric_user)
+    if accept:
+        print("Voce acertou!")
+    else:
+        if numeric_great:
+            print("Maior que o numero do sorteio")
+        elif numeric_minor:
+            print("O numero e o menor que sorteio")
+    round_game += 1
 
 print("Fim do jogo!")
