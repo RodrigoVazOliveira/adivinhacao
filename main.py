a9a947731b3f7be0ea421eaf0eb00c38b2afc384
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-12 19:07:47 -0300

    colocar o main

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ print("========================================")
 print("========== Bem vindo ===================")
 print("========================================")
 
-print("Escolha o jogo")
-print("(1) Adivinhacao\n(2) Verificar usuario do sistema")
-number_game = int(input("Por favor, digite a sua escolha: "))
 
-if number_game == 1:
-    advinhacao.advinha()
-elif number_game == 2:
-    user_system.check_user()
+def execute():
+    print("Escolha o jogo")
+    print("(1) Adivinhacao\n(2) Verificar usuario do sistema")
+    number_game = int(input("Por favor, digite a sua escolha: "))
+
+    if number_game == 1:
+        advinhacao.execute()
+    elif number_game == 2:
+        user_system.check_user()
+
+
+if __name__ == "__main__":
+    execute()
