2fc696cfa0f495b1185f585d437684a0ab350e23
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-12 11:03:33 -0300

    Usar break e continue

--- advinhacao.py
+++ advinhacao.py
@@ -10,14 +10,19 @@ round_game = 1
 # while round_game <= numeric_attempt:
 for round_game in range(1, numeric_attempt + 1):
     print("rodada {}, numero de tentativas {}".format(round_game, numeric_attempt))
-    numeric_user = int(input("Digite um o seu numero: "))
+    numeric_user = int(input("Digite um o seu numero de 1 a 100: "))
     accept = numeric_secret == numeric_user
     numeric_great = numeric_user > numeric_secret
     numeric_minor = numeric_user < numeric_secret
 
+    if numeric_user < 1 or numeric_user > 100:
+        print("Voce deve digitar um numero entre 1 a 100!")
+        continue
+
     print("Voce digitou ", numeric_user)
     if accept:
         print("Voce acertou!")
+        break
     else:
         if numeric_great:
             print("Maior que o numero do sorteio")
