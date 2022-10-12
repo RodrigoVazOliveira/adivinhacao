ce32114a8ab74f02de776d5dd48c9a162083d6e8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-12 19:04:45 -0300

    Melhorar o codigo do jogo

--- advinhacao.py
+++ advinhacao.py
@@ -1,52 +1,91 @@
 import random
 
-def advinha():
+
+def to_define_level(level):
+    if level == 1:
+        return 20
+    elif level == 2:
+        return 10
+    else:
+        return 5
+
+
+def get_level_game():
+    print("Qual o nivel de dificuldade?")
+    print("(1) Facil \n(2) Medio \n(3) Dificil")
+    return int(input("Escolha o nivel: "))
+
+
+def is_inside_interval(numeric_user):
+    if numeric_user < 1 or numeric_user > 100:
+        print("Voce deve digitar um numero entre 1 a 100!")
+        return True
+    return False
+
+
+def get_numeric_attempt_game(round_game, numeric_attempt):
+    print("rodada {}, numero de tentativas {}".format(round_game, numeric_attempt))
+    return int(input("Digite um o seu numero de 1 a 100: "))
+
+
+def user_not_accept(numeric_great,
+                    numeric_minor,
+                    round_game,
+                    numeric_attempt,
+                    numeric_secret, points):
+    if numeric_great:
+        print("Maior que o numero do sorteio")
+        if round_game == numeric_attempt:
+            print("o numero secreto era {}, voce fez {}.".format(numeric_secret, points))
+    elif numeric_minor:
+        print("O numero e o menor que sorteio")
+        if round_game == numeric_attempt:
+            print("o numero secreto era {}, voce fez {}.".format(numeric_secret, points))
+
+
+def adjust_points(numeric_secret, numeric_user):
+    points_loser = abs(numeric_secret - numeric_user)
+    return points_loser
+
+
+def run_new_game(round_game,
+                 numeric_attempt,
+                 numeric_secret,
+                 points,
+                 numeric_user):
+    # is_inside_interval(numeric_user)
+    accept = numeric_secret == numeric_user
+    numeric_great = numeric_user > numeric_secret
+    numeric_minor = numeric_user < numeric_secret
+
+    print("Voce digitou ", numeric_user)
+    if accept:
+        print("Voce acertou! Sua pontuacao total e de {}".format(points))
+        return True
+    else:
+        user_not_accept(numeric_great, numeric_minor, round_game, numeric_attempt, numeric_secret, points)
+        return False
+
+
+def execute():
     print("=================================")
     print("Bem vindo ao jogo de adivinhacao!")
     print("=================================")
-
     numeric_secret = round(random.randrange(1, 101))
-    numeric_attempt = 3
     round_game = 1
     points = 1000
+    level = get_level_game()
+    numeric_attempt = to_define_level(level)
 
-    print("Qual o nivel de dificuldade?")
-    print("(1) Facil \n(2) Medio \n(3) Dificil")
-    level = int(input("Escolha o nivel: "))
-
-    if level == 1:
-        numeric_attempt = 20
-    elif level == 2:
-        numeric_attempt = 10
-    else:
-        numeric_attempt = 5
-
-    # while round_game <= numeric_attempt:
     for round_game in range(1, numeric_attempt + 1):
-        print("rodada {}, numero de tentativas {}".format(round_game, numeric_attempt))
-        numeric_user = int(input("Digite um o seu numero de 1 a 100: "))
-        accept = numeric_secret == numeric_user
-        numeric_great = numeric_user > numeric_secret
-        numeric_minor = numeric_user < numeric_secret
-
-        if numeric_user < 1 or numeric_user > 100:
-            print("Voce deve digitar um numero entre 1 a 100!")
-            continue
-
-        print("Voce digitou ", numeric_user)
-        if accept:
-            print("Voce acertou! Sua pontuacao total e de {}".format(points))
+        numeric_user = get_numeric_attempt_game(round_game, numeric_attempt)
+        result_game = run_new_game(round_game, numeric_attempt, numeric_secret, points, numeric_user)
+        points = points - adjust_points(numeric_secret, numeric_user)
+        if result_game:
             break
-        else:
-            if numeric_great:
-                print("Maior que o numero do sorteio")
-                if round_game == numeric_attempt:
-                    print("o numero secreto era {}, voce fez {}.".format(numeric_secret, points))
-            elif numeric_minor:
-                print("O numero e o menor que sorteio")
-                if round_game == numeric_attempt:
-                    print("o numero secreto era {}, voce fez {}.".format(numeric_secret, points))
-            points_loser = abs(numeric_secret - numeric_user)
-            points -= points_loser
 
     print("Fim do jogo!")
+
+
+if __name__ == "__main__":
+    execute()
