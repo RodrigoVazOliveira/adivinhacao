2da5eda68b5be2021c640265c3ca2e120a433e9f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-10-26 02:32:07 -0300

    calcular total de funcionarios

---
+++ names_employers.py
@@ -0,0 +1,8 @@
+def show_total_employers():
+    employers = ['Astrid','Flavia','Talia', ... ,'Mauricio', 'Waldemar', 'Marina']
+    total_employer = len(employers)
+    print("Total de funcionarios: {}".format(total_employer))
+
+
+if __name__ == "__main__":
+    show_total_employers()
